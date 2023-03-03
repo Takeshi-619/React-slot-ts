f3baa6f7944cde0f4989e75c2d00ec94321c17ac
Takeshi-619 <take.a.0619@icloud.com>
2023-03-04 02:49:23 +0900

    uodate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,56 @@
 import "./App.scss";
+import { useState, useRef } from "react";
+import ReelBar from "./components/ReelBar";
 
 function App() {
-  return <div className=""></div>;
+  const [data1, setData1] = useState([3, 2, 1]);
+  const [data2, setData2] = useState([3, 2, 1]);
+  const [data3, setData3] = useState([3, 2, 1]);
+  // const [credit, setCredit] = useState(0);
+  // const timer1 = useRef();
+  // const timer2 = useRef();
+  // const timer3 = useRef();
+  const leftReel = useRef();
+  const centerReel = useRef();
+  const rigthReel = useRef();
+  // const [judge1, setJudge1] = useState(false);
+  // const [judge2, setJudge2] = useState(false);
+  // const [judge3, setJudge3] = useState(false);
+  // const [open, setOpen] = useState(false);
+  // const myLength = 7;
+  return (
+    <div className="slot">
+      <div className="slot_content">
+        <div className="slot_content_chance">
+          <img src="/img/bigChance.png" alt="" />
+        </div>
+
+        <div className="slot_content_luckeyNumber">
+          <img src="/img/luckyNum@2x.png" alt="" />
+        </div>
+
+        <div className="slot_content_reel">
+          <ReelBar />
+          <ReelBar />
+          <ReelBar />
+        </div>
+
+        <div className="slot_content_ramp"></div>
+
+        <div className="slot_content_credit"></div>
+
+        <div className="slot_content_btns">
+          <button className="slot_content_btns_lever"></button>
+
+          <div className="slot_content_btns_cover">
+            <button></button>
+            <button></button>
+            <button></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ReelBar.tsx
@@ -0,0 +1,21 @@
+import img1 from "/img/number/777.png";
+import img2 from "/img/number/budou.png";
+import img3 from "/img/number/chery.png";
+import img4 from "/img/number/piero_1.png";
+import img5 from "/img/number/sai.png";
+import img6 from "/img/number/suzu.png";
+import img7 from "/img/number/taiger.png";
+
+const ReelBar = () => {
+  const imgs = [img1, img2, img3, img4, img5, img6, img7];
+
+  return (
+    <div className={""}>
+      <div className="reelP">
+        <ul></ul>
+      </div>
+    </div>
+  );
+};
+
+export default ReelBar;
